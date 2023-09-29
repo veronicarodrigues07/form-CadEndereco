ee6714aa24ff753ec1d6f3be2088c4088a0e5044
Veronica Rodrigues <veronicaizabellyrodrigues12@gmail.com>
2023-09-29 11:45:35 -0300

    Função que preenche formulario via cep

--- controller.js
+++ controller.js
@@ -12,4 +12,13 @@ document.getElementById('estado').value='';
 
 //Verifica se CEP é valido
 const eNumero = (numero) => /^[0-9]+$/.test(numero); //testa número informado com expressão regular
-const cepValido =(cep) => cep.length == 8 && eNumero(cep);
+const cepValido =(cep) => cep.length == 8 && eNumero(cep);// Verificar o tamanho do cep digitando e executando sua função de validação do cep eNumero
+
+//Função para preencher formulario 
+const preencherFormulario = (endereco) =>{
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endeco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+
+}

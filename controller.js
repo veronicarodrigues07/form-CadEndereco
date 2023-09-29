356f8d9ef016dc61e1b64ecd6c5033d4d32232c4
Veronica Rodrigues <veronicaizabellyrodrigues12@gmail.com>
2023-09-29 11:08:32 -0300

    criamos função de limpar formulario

--- controller.js
+++ controller.js
@@ -1,1 +1,11 @@
 'use strict'; // Modo restrito
+//Consumindo API de CEP, do ViaCep
+//https://viacep.com.br/
+
+// Função para limpar o formulário
+ const limparFormulario = () => {
+document.getElementById('rua').value='';
+document.getElementById('bairro').value='';
+document.getElementById('cidade').value='';
+document.getElementById('estado').value='';
+}

1699a3695196df67f17a7411850bc669a81b4058
Veronica Rodrigues <veronicaizabellyrodrigues12@gmail.com>
2023-10-04 11:18:57 -0300

    adicionando inforações

--- controller.js
+++ controller.js
@@ -1,44 +1,42 @@
 'use strict'; // Modo restrito
-//Consumindo API de CEP, do ViaCep
-//https://viacep.com.br/
+// Consumindo API de CEP, do Viacep
+// https://viacep.com.br/
 
 // Função para limpar o formulário
- const limparFormulario = () => {
-document.getElementById('rua').value='';
-document.getElementById('bairro').value='';
-document.getElementById('cidade').value='';
-document.getElementById('estado').value='';
+const limparFormulario = () => {
+    document.getElementById('rua').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
 }
 
 //Verifica se CEP é valido
-const eNumero = (numero) => /^[0-9]+$/.test(numero); //testa número informado com expressão regular
-const cepValido =(cep) => cep.length == 8 && eNumero(cep);// Verificar o tamanho do cep digitando e executando sua função de validação do cep eNumero
+const eNumero = (numero) => /^[0-9]+$/.test(numero); //testa numero informado com expressão regular
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);//verifica tamanho do cep digitado e executa função de validação do cep eNumero
 
-//Função para preencher formulario 
-const preencherFormulario = (endereco) =>{
+//função para preencher formulário
+const preencherFormulario = (endereco) => {
     document.getElementById('rua').value = endereco.logradouro;
     document.getElementById('bairro').value = endereco.bairro;
-    document.getElementById('cidade').value = endeco.localidade;
+    document.getElementById('cidade').value = endereco.localidade;
     document.getElementById('estado').value = endereco.uf;
-
 }
 
-//Consumo da API da ViaCEP
- const pesquisarCep = async()=>{
+//consumo da API da ViaCEP
+const pesquisarCep = async() => {
     limparFormulario();
     const url = `http://viacep.com.br/ws/${cep.value}/json/`;
     if(cepValido(cep.value)){
-        const dados =await fetch(url); //aguardar
-        const addres = await dados.json(); 
-        //has0wnProperty retorna um booleano indicando se o objeto possui a propriedade especifica como uma propriedade definida no proprio objeto em questao
+        const dados = await fetch(url); //esperar
+        const addres = await dados.json();
         if(addres.hasOwnProperty('erro')){
-            alert('CEP não encontrado');
+            alert('CEP não encontrado')
         }else{
-            preencherFormulario(addres); 
+            preencherFormulario(addres);
         }
     }else{
-        alert('CEP Incorreto');
+        alert('CEP incorreto')
     }
- }
- //Adicona evento DOM ao input do CEP para executar função pesquisarCep
- document.getElementById('cep').addEventListener('focusout',pesquisarCep);
+}
+//adiciona evento DOM ao input do CEP para executar função pesquisarCep
+document.getElementById('cep').addEventListener('focusout', pesquisarCep);
